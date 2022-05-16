d0ee918e756a2a131345c0084cd9c4ade7638f28
mojtabahanaei1400 <mojtabahanaei1400@gamail.com>
2022-05-16 11:08:34 +0430

    salam

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,14 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!ششش");
-            Console.WriteLine("Hello World!ششش");
-            Console.WriteLine("Hello World!ششش");
-            Console.WriteLine("Hello World!ششش"); Console.WriteLine("Hello World!ششش");
-            Console.WriteLine("Hello World!ششش"); Console.WriteLine("Hello World!ششش");
-            Console.WriteLine("Hello World!ششش"); Console.WriteLine("Hello World!ششش");
+            
 
 
 
